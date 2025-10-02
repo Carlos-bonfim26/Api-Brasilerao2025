cf8ec6f549e805561f64d68c05b72d2561c01b29
carlosbonfim <carlosbonfim722@gmail.com>
2025-10-02 11:38:46 -0300

    Build: tabela do brasileirão 2025 base

---
+++ tabela.js
@@ -0,0 +1,170 @@
+const tabela2025 = [
+  {
+    nome: 'Atlético Mineiro',
+    sigla: 'ATL',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Bahia',
+    sigla: 'BAH',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Botafogo',
+    sigla: 'BOT',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Ceará',
+    sigla: 'CEA',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Corinthians',
+    sigla: 'COR',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Cruzeiro',
+    sigla: 'CRU',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Flamengo',
+    sigla: 'FLA',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Fluminense',
+    sigla: 'FLU',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Fortaleza',
+    sigla: 'FOR',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Grêmio',
+    sigla: 'GRE',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Internacional',
+    sigla: 'INT',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+  {
+    nome: 'Juventude',
+    sigla: 'JUV',
+    pontos: 0,
+    jogos: 0,
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    golsMarcados: 0,
+    golsSofridos: 0,
+    saldoGols: function () {
+      return this.golsMarcados - this.golsSofridos;
+    }
+  },
+];
