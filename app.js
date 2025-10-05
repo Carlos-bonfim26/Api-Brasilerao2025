1af408210b08fb56c6b4bfa772234e9e1eae04bd
carlosbonfim <carlosbonfim722@gmail.com>
2025-10-04 22:36:44 -0300

    feat: requisição post e delete adicionadas na API

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-import tabela2025 from "./tabela.js";  
+import tabela2025 from "./tabela.js";
 import express from "express";
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.get("/", (req, res)=>{
-    res.send(tabela2025);
-
-})
-app.get('/:sigla', (req, res) => {
-    const sigla = req.params.sigla.toUpperCase();
-    const equipe = tabela2025.find(item => item.sigla === sigla);
-    if (equipe) {
-        res.send(equipe);
-    } else {
-        res.status(404).send({ error: "Equipe não encontrada" });
-    }
+app.get("/", (req, res) => {
+  res.send(tabela2025);
 });
-app.listen(port, ()=> console.log(`Servidor rodando na porta ${port}`));
+app.get("/:sigla", (req, res) => {
+  const sigla = req.params.sigla.toUpperCase();
+  const equipe = tabela2025.find((item) => item.sigla === sigla);
+  if (equipe) {
+    res.send(equipe);
+  } else {
+    res.status(404).send({ error: "Equipe não encontrada" });
+  }
+});
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
 
-app.put('/:sigla', (req,res)=>{
-    const sigla = req.params.sigla.toUpperCase();
-    const timeSelecionado = tabela2025.find(time => time.sigla === sigla);
-    const campos = Object.keys(req.body);
-    for(let campo of campos){
-        timeSelecionado[campo] = req.body[campo];
-    }
-    res.status(200).send(timeSelecionado);
-
-})
+app.put("/:sigla", (req, res) => {
+  const sigla = req.params.sigla.toUpperCase();
+  const timeSelecionado = tabela2025.find((time) => time.sigla === sigla);
+  const campos = Object.keys(req.body);
+  for (let campo of campos) {
+    timeSelecionado[campo] = req.body[campo];
+  }
+  res.status(200).send(timeSelecionado);
+});
+app.post("/", (req, res) => {
+  const novoTime = req.body;
+  tabela2025.push(novoTime);
+  res.status(201).send(novoTime);
+});
+app.delete("/:sigla", (req, res) => {
+  const sigla = req.params.sigla.toUpperCase();
+  const timeSelecionado = tabela2025.find((time) => time.sigla === sigla);
+  const index = tabela2025.indexOf(timeSelecionado);
+  if (index !== -1) {
+    const timeRemovido = tabela2025.splice(index, 1);
+    res.status(200).send(timeRemovido[0]);
+  } else {
+    res.status(404).send({ error: "Equipe não encontrada" });
+  }
+  res.status(200).send(timeRemovido);
+});
